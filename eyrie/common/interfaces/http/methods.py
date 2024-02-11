e1b2dc3d4775c850f05179320863fff52e007c87
Retr0327 <retr0327.dev@gmail.com>
2024-02-11 22:09:42 +0800

    Define HTTP method interface

---
+++ eyrie/common/interfaces/http/methods.py
@@ -0,0 +1,13 @@
+from typing import Literal
+
+HTTPMethodType = Literal[
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "PATCH",
+    "OPTIONS",
+    "HEAD",
+    "TRACE",
+    "CONNECT",
+]
