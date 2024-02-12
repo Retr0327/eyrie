6c4075958e7c194a414de0152351bb99b380c9eb
Retr0327 <retr0327.dev@gmail.com>
2024-02-12 19:12:08 +0800

    Export route handler decorators

---
+++ eyrie/common/decorators/http/__init__.py
@@ -0,0 +1,25 @@
+from .request_mapping import (
+    Connect,
+    Delete,
+    Get,
+    Head,
+    Options,
+    Patch,
+    Post,
+    Put,
+    Trace,
+    create_mapping_decorator,
+)
+
+__all__ = [
+    "Connect",
+    "Delete",
+    "Get",
+    "Head",
+    "Options",
+    "Patch",
+    "Post",
+    "Put",
+    "Trace",
+    "create_mapping_decorator",
+]
